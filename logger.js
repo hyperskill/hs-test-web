f7e9039380bda23a2641e727d78a454c3496e3f1
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-12-19 00:06:25 +0300

    Add basic tests

--- logger.js
+++ logger.js
@@ -1,23 +1,39 @@
-let failedMessage = '#educational_plugin FAILED + ';
-let eduMessage = '#educational_plugin ';
+const failedMessage = '#educational_plugin FAILED + ';
+const eduMessage = '#educational_plugin ';
 
 function failed(message) {
     let lines = message.split('\n');
-    console.log('\n' + failedMessage + lines[0]);
+    console.log('\n' + failedMessage + lines[0] + '\n');
     for (let i = 1; i < lines.length; i++) {
-        console.log(eduMessage + lines[i]);
+        console.log(eduMessage + lines[i] + '\n');
     }
 }
 
 function passed() {
-    console.log('\n' + eduMessage + 'test OK');
+    console.log('\n' + eduMessage + 'test OK' + '\n');
 }
 
-function disableEduToolsMessages() {
-    failedMessage = '';
-    eduMessage = '';
+function filterLine(line) {
+    if (line.startsWith(failedMessage)) {
+        return line.substring(failedMessage.length);
+    }
+    if (line.startsWith(eduMessage)) {
+        return line.substring(eduMessage.length);
+    }
+    return line;
+}
+
+function filter(message) {
+    message = message.replace("\n" + failedMessage, '');
+    message = message.replace("\n" + eduMessage + "test OK", 'test OK');
+
+    while (message.includes(eduMessage)) {
+        message = message.replace(eduMessage, '');
+    }
+
+    return message;
 }
 
 exports.failed = failed
 exports.passed = passed
-exports.disableEduToolsMessages = disableEduToolsMessages
+exports.filter = filter
--- stageTest.test.js
+++ stageTest.test.js
@@ -1,10 +1,65 @@
 const logger = require('./logger');
+const hs = require('./stageTest');
 
-beforeAll(() => {
-    logger.disableEduToolsMessages();
+let logged = '';
+let savedConsole;
+
+beforeEach(() => {
+    logged = '';
+    savedConsole = console.log;
+    console.log = msg => {
+        logged += logger.filter(msg);
+    }
+});
+
+afterEach(() => {
+    console.log = savedConsole;
 });
 
 
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
+test('Accepted single test', () => {
+    hs.test(
+        () => hs.accept()
+    );
+    expect(logged).toBe('test OK\n');
+});
+
+test('Accepted two tests', () => {
+    hs.test(
+        () => hs.accept(),
+        () => hs.accept(),
+    );
+    expect(logged).toBe('test OK\n');
+});
+
+test('Wrong answer test 1', () => {
+    hs.test(
+        () => hs.wrong(),
+        () => hs.accept(),
+    );
+    expect(logged).toBe('Wrong answer in test #1\n');
+});
+
+test('Wrong answer test 2', () => {
+    hs.test(
+        () => hs.accept(),
+        () => hs.wrong(),
+    );
+    expect(logged).toBe('Wrong answer in test #2\n');
+});
+
+test('Wrong answer with feedback test 1', () => {
+    hs.test(
+        () => hs.wrong('Write better code'),
+        () => hs.wrong(),
+    );
+    expect(logged).toBe('Wrong answer in test #1\n\nWrite better code\n');
+});
+
+test('Wrong answer with feedback test 2', () => {
+    hs.test(
+        () => hs.accept(),
+        () => hs.wrong('Please write better code'),
+    );
+    expect(logged).toBe('Wrong answer in test #2\n\nPlease write better code\n');
 });
