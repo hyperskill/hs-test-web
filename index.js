e2c81f983fa4df381cc1a5ca64d65fac0e2c10c9
aaaaaa2493 <aaaaaa2493@yandex.ru>
2019-12-18 18:23:23 +0300

    Add simplest tests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 
 function failed(message) {
     let lines = message.split('\n');
@@ -11,6 +12,24 @@ function passed() {
     console.log('\n#educational_plugin test OK');
 }
 
+function test(...tests) {
+    let isFailed = false;
+    for (let testNum = 0; testNum < tests.length; testNum++) {
+        let currTest = tests[testNum];
+        let file = fs.readFileSync(currTest, 'utf8');
+        file = file.trim();
+        if (file.length === 0) {
+            let message = 'Wrong answer in test #' + (testNum + 1);
+            message += '\n\nFile ' + currTest + ' should not be empty!'
+            failed(message);
+            isFailed = true;
+            break;
+        }
+    }
+    if (!isFailed) {
+        passed();
+    }
+}
 
-exports.failed = failed;
-exports.passed = passed;
+
+exports.test = test;
