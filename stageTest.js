cdc6645dcd69fddd89051d45622048f4d9638a98
Ainur Gimadeev <inuur@mail.ru>
2021-08-11 17:20:00 +0300

    Move back to Jest. Async run of the puppeeter browser

--- stageTest.js
+++ stageTest.js
@@ -1,29 +1,86 @@
-import puppeteer from "puppeteer"
+const puppeteer = require("puppeteer")
 
-export default class StageTest {
+class StageTest {
 
     browser = null;
+    tests = [];
 
-    async _runBrowser() {
-        this.browser = await puppeteer.launch({
+    static async new() {
+        const browser = await puppeteer.launch({
             headless: false,
             defaultViewport: null,
             args: ['--start-maximized', '--disable-infobar'],
             ignoreDefaultArgs: ['--enable-automation'],
             devtools: true
         });
+        return new this(browser)
+    }
+
+    constructor(browser) {
+        this.browser = browser
+    }
+
+    async newPage(path) {
+        const page = await this.browser.newPage();
+        await page.goto(path);
+        return page;
     }
 
     async _closeBrowser() {
         await this.browser.close();
     }
 
+    async onPage(page, test) {
+        const _page = await page
+        _page.on('console', msg => console.log(msg.text()));
+        return async () => {
+            return await _page.evaluate(test)
+        }
+    }
+
+    async onNewPage(path, test) {
+        const page = await this.newPage(path)
+        page.on('console', msg => console.log(msg.text()));
+        return async () => {
+            const result = await page.evaluate(test)
+            await page.close();
+            return result
+        }
+    }
+
+    async executeTestCases() {
+        if (this.tests.length === 0) {
+            throw new Error("UE: No tests found")
+        }
+
+        const result = await this.tests.filter(async (currentTest) => {
+            return await typeof currentTest !== 'function'
+        })
+
+        for (let i = 0; i < this.tests.length; i++) {
+            const currentTest = await this.tests[i];
+
+            if (typeof currentTest !== 'function') {
+                throw new Error("UE: Found wrong test case that is not a function");
+            }
+
+            await currentTest()
+        }
+    }
+
     async _runTests() {
-        await this._runBrowser()
-        await this._closeBrowser()
+        try {
+            await this.executeTestCases()
+        } catch (err) {
+            throw err;
+        } finally {
+            await this._closeBrowser()
+        }
     }
 
     async runTests() {
         await this._runTests()
     }
 }
+
+exports.StageTest = StageTest;
