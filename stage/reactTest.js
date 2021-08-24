e3aa8da321d554fa3e0ac828155b69dcbcb34c1b
Ainur Gimadeev <inuur@mail.ru>
2021-08-24 17:12:20 +0300

    Remove parametrs from ReactTest constructor

--- stage/reactTest.js
+++ stage/reactTest.js
@@ -1,13 +1,22 @@
 const {StageTest} = require("./stageTest.js")
 const {UnexpectedError} = require("../exception/unexpected_error.js")
+const callsite = require('callsite');
+const path = require('path')
 
 class ReactTest extends StageTest {
 
-    constructor(host, port, dirPath) {
+    host = 'localhost'
+    port = 31328
+
+    constructor() {
         super();
         try {
             const {ReactRunner} = require("../testing/runner/react_runner.js")
-            this.runner = new ReactRunner(host, port, dirPath)
+            const stack = callsite(),
+                requester = stack[1].getFileName();
+            const path_folders = requester.split(path.sep)
+            const resultPath = path_folders.splice(0, path_folders.length - 2).join(path.sep)
+            this.runner = new ReactRunner(this.host, this.port, resultPath)
         } catch (err) {
             throw new UnexpectedError("Required dependencies are not installed to test React applications", err)
         }
