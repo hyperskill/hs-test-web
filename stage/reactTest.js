a9ffa8a0239c54c23761d41c9b6500986784acab
Ainur Gimadeev <inuur@mail.ru>
2021-08-24 16:56:00 +0300

    Remove initiation of webpack from pure js project

--- stage/reactTest.js
+++ stage/reactTest.js
@@ -1,11 +1,16 @@
 const {StageTest} = require("./stageTest.js")
-const {ReactRunner} = require("../testing/runner/react_runner.js")
+const {UnexpectedError} = require("../exception/unexpected_error.js")
 
 class ReactTest extends StageTest {
 
     constructor(host, port, dirPath) {
         super();
-        this.runner = new ReactRunner(host, port, dirPath)
+        try {
+            const {ReactRunner} = require("../testing/runner/react_runner.js")
+            this.runner = new ReactRunner(host, port, dirPath)
+        } catch (err) {
+            throw new UnexpectedError("Required dependencies are not installed to test React applications", err)
+        }
     }
 }
 
