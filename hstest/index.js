c369752a2a094bce69b0af06d6c664d07eb58957
Ainur Gimadeev <inuur@mail.ru>
2021-08-31 15:37:50 +0300

    Support deprecated tests

--- hstest/index.js
+++ hstest/index.js
@@ -2,9 +2,13 @@ const {StageTest} = require("./stage/stageTest.js")
 const {ReactTest} = require("./stage/reactTest.js")
 const {CheckResult} = require("./outcome/check_result.js")
 
-module.exports = {
-    StageTest,
-    ReactTest,
-    wrong: CheckResult.wrong,
-    correct: CheckResult.correct
+if (global.isNewTest) {
+    module.exports = {
+        StageTest,
+        ReactTest,
+        wrong: CheckResult.wrong,
+        correct: CheckResult.correct
+    }
+} else {
+    module.exports = require("./oldStageTest.js")
 }
