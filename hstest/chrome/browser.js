095cd3d4ebb3005023c20c0fb742e307cc6e3f51
Ainur Gimadeev <inuur@mail.ru>
2021-12-06 00:07:06 +0300

    Remove trim()

--- hstest/chrome/browser.js
+++ hstest/chrome/browser.js
@@ -11,7 +11,7 @@ class Browser {
 
         let additionalBrowserLaunchArgs = {}
 
-        if (process.env.NODE_ENV.trim() === 'testlib') {
+        if (process.env.NODE_ENV === 'testlib') {
             additionalBrowserLaunchArgs['headless'] = true
         }
 
