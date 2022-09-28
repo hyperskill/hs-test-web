f0dd108b16e58eac128c9b24b11346f4a48bd77e
Alexander Petrov <meanmail@meanmail.ru>
2022-09-28 21:38:57 +0400

    Fix lint (#8)

--- dist/hstest/handler/eventHandler.js
+++ dist/hstest/handler/eventHandler.js
@@ -2,28 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class EventHandler {
     static async waitForEvent(eventName, pageInstance, elementHandle, timeout = 10000) {
-        return pageInstance.evaluate((event, elementHandle, timeout) => {
-            const sleep = (ms) => {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            };
+        async function pageFunction(event, elementHandle, timeout) {
             const element = elementHandle || window;
             let isEventHappened = false;
-            element.addEventListener(event, () => {
+            function listener() {
                 isEventHappened = true;
-            });
-            return new Promise(async (resolve) => {
-                let isWaiting = true;
-                const waiter = setTimeout(() => {
-                    isWaiting = false;
-                    resolve(false);
-                }, timeout);
-                while (!isEventHappened && isWaiting) {
-                    await sleep(200);
+            }
+            function sleep(ms) {
+                return new Promise(resolve => {
+                    setTimeout(resolve, ms);
+                });
+            }
+            const sleepTimeout = 200;
+            element.addEventListener(event, listener);
+            try {
+                const startTime = performance.now();
+                const checkTimeout = () => {
+                    return (performance.now() - startTime) < (timeout - sleepTimeout);
+                };
+                while (!isEventHappened) {
+                    if (!checkTimeout()) {
+                        return false;
+                    }
+                    await sleep(sleepTimeout);
                 }
-                clearTimeout(waiter);
-                resolve(true);
-            });
-        }, eventName, elementHandle, timeout);
+                return true;
+            }
+            finally {
+                element.removeEventListener(event, listener);
+            }
+        }
+        return pageInstance.evaluate(pageFunction, eventName, elementHandle, timeout);
     }
 }
 exports.default = EventHandler;
--- hstest/handler/eventHandler.ts
+++ hstest/handler/eventHandler.ts
@@ -1,32 +1,45 @@
 import puppeteer from "puppeteer";
 
 class EventHandler {
-    static async waitForEvent(eventName: string, pageInstance: puppeteer.Page, elementHandle: puppeteer.ElementHandle | null, timeout = 10000): Promise<any> {
-        return pageInstance.evaluate((event, elementHandle, timeout) => {
-            const sleep = (ms: number) => {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            };
-
+    static async waitForEvent(eventName: string, pageInstance: puppeteer.Page,
+                              elementHandle: puppeteer.ElementHandle | null,
+                              timeout = 10000): Promise<any> {
+        async function pageFunction (event: string, elementHandle: HTMLElement,
+                                     timeout: number): Promise<boolean> {
             const element = elementHandle || window;
             let isEventHappened = false;
 
-            element.addEventListener(event, () => {
+            function listener () {
                 isEventHappened = true;
-            });
+            }
+            function sleep (ms: number) {
+                return new Promise(resolve => {
+                    setTimeout(resolve, ms);
+                });
+            }
+            const sleepTimeout = 200;
 
-            return new Promise(async resolve => {
-                let isWaiting = true;
-                const waiter = setTimeout(() => {
-                    isWaiting = false;
-                    resolve(false);
-                }, timeout);
-                while (!isEventHappened && isWaiting) {
-                    await sleep(200);
+            element.addEventListener(event, listener);
+
+            try {
+                const startTime = performance.now();
+                const checkTimeout = (): boolean => {
+                    return (performance.now() - startTime) < (timeout - sleepTimeout);
+                };
+
+                while (!isEventHappened) {
+                    if (!checkTimeout()) {
+                      return false;
+                    }
+                    await sleep(sleepTimeout);
                 }
-                clearTimeout(waiter);
-                resolve(true);
-            });
-        }, eventName, elementHandle, timeout);
+                return true;
+            } finally {
+                element.removeEventListener(event, listener);
+            }
+        }
+
+        return pageInstance.evaluate(pageFunction, eventName, elementHandle, timeout);
     }
 }
 
