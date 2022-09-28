5b2bcd9cdd70c4ec2ea065d4f38ab470bd122546
meanmail <meanmail@meanmail.ru>
2022-09-28 21:14:16 +0400

    Fix lint

--- hstest/handler/eventHandler.ts
+++ hstest/handler/eventHandler.ts
@@ -1,32 +1,48 @@
 import puppeteer from "puppeteer";
 
-class EventHandler {
-    static async waitForEvent(eventName: string, pageInstance: puppeteer.Page, elementHandle: puppeteer.ElementHandle | null, timeout = 10000): Promise<any> {
-        return pageInstance.evaluate((event, elementHandle, timeout) => {
-            const sleep = (ms: number) => {
-                return new Promise(resolve => setTimeout(resolve, ms));
-            };
 
+function sleep (ms: number) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function checkTimeout(startTime: number, timeout: number): boolean {
+    return (performance.now() - startTime) < timeout;
+}
+
+class EventHandler {
+    static async waitForEvent(eventName: string, pageInstance: puppeteer.Page,
+                              elementHandle: puppeteer.ElementHandle | null,
+                              timeout = 10000): Promise<any> {
+        async function pageFunction (event: string, elementHandle: HTMLElement,
+                                     timeout: number): Promise<boolean> {
             const element = elementHandle || window;
             let isEventHappened = false;
 
-            element.addEventListener(event, () => {
+            function listener () {
                 isEventHappened = true;
-            });
+            }
+            const sleepTimeout = 200;
 
-            return new Promise(async resolve => {
-                let isWaiting = true;
-                const waiter = setTimeout(() => {
-                    isWaiting = false;
-                    resolve(false);
-                }, timeout);
-                while (!isEventHappened && isWaiting) {
-                    await sleep(200);
+            element.addEventListener(event, listener);
+
+            try {
+                const start = performance.now();
+
+                while (!isEventHappened) {
+                    if (!checkTimeout(start, timeout - sleepTimeout)) {
+                      return false;
+                    }
+                    await sleep(sleepTimeout);
                 }
-                clearTimeout(waiter);
-                resolve(true);
-            });
-        }, eventName, elementHandle, timeout);
+                return true;
+            } finally {
+                element.removeEventListener(event, listener);
+            }
+        }
+
+        return pageInstance.evaluate(pageFunction, eventName, elementHandle, timeout);
     }
 }
 
