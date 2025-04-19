9e07b2f1a04b85fca4afe247a8ef4cd392d3be2d
polischuks <polischuk0107@gmail.com>
2025-04-19 19:20:55 +0300

    refactor: replace chai.expect with expect for consistency in test files

--- tests/vanilla/outcome/unexpectedError/unexpectedErrorOnRuntimeError.test.mjs
+++ tests/vanilla/outcome/unexpectedError/unexpectedErrorOnRuntimeError.test.mjs
@@ -1,5 +1,5 @@
-import {StageTest, correct, wrong} from "../../../../dist/hstest/index.js"
-import chai from 'chai'
+import {StageTest, correct} from "../../../../dist/hstest/index.js"
+import { expect } from 'chai'
 
 class UnexpectedErrorOnRuntimeErrorTest extends StageTest {
     tests = [
@@ -14,8 +14,8 @@ it('Test unexpected error message on runtime error', async () => {
     try {
         await new UnexpectedErrorOnRuntimeErrorTest().runTests()
     } catch (err) {
-        chai.expect(err.toString()).contain("Unexpected error in test #1\n\nWe have recorded this bug and will fix it soon.\n\n")
-        chai.expect(err.toString()).contain("ReferenceError: document is not defined")
+        expect(err.toString()).contain("Unexpected error in test #1\n\nWe have recorded this bug and will fix it soon.\n\n")
+        expect(err.toString()).contain("ReferenceError: document is not defined")
         return
     }
     throw new Error("The test should fail with unexpected error!")
